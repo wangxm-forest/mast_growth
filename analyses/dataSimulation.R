9094327a0efb612b1f3340328a0e6805c7572c25
wangxm-forest <wangxiaomao0209@gmail.com>
2026-03-21 13:12:21 -0700

    Added data simulation for the generative model

--- analyses/dataSimulation.R
+++ analyses/dataSimulation.R
@@ -32,9 +32,10 @@ beta_GST <- 0.5
 #beta_GSP <- 0.3
 beta_growth1 <- 0.04
 beta_growth2 <- 3.2
-gamma <- 0.01
-phi <- 2
-sigma <- 0.2
+theta <- 0.01
+phi_sc <- 2
+sigma_rw <- 0.5
+
 
 C <- alpha + alpha_site[site] + beta_dbh * DBH + beta_GST * GST
 
@@ -42,11 +43,11 @@ frac_R <- runif(N, 0.1, 0.5)
 R <- C * frac_R
 G <- C - R 
 
-sc_pred <- R / gamma
-sc <- rnegbin(N, mu = sc_pred, theta = phi)
+sc_pred <- R / theta
+sc <- rnegbin(N, mu = sc_pred, theta = phi_sc)
 
 rw_pred <- (((G / beta_growth1 + DBH^beta_growth2)^(1 / beta_growth2)) - DBH) / 2
-rw <- rlnorm(N, meanlog = log(rw_pred), sdlog = sigma)
+rw <- rlnorm(N, meanlog = log(rw_pred), sdlog = sigma_rw)
 
 # Data
 stanData <- list(
@@ -83,19 +84,21 @@ par(mfrow = c(3,3))
 util$plot_div_pairs(names, names, samples, diagnostics)
 dev.off()
 
-pdf("figures/priorPosteriorPlot.pdf", height = 9, width = 9)
+pdf("figures/priorPosteriorPlotBasic.pdf", height = 9, width = 9)
 par(mfrow = c(3,3))
 util$plot_expectand_pushforward(samples[['alpha']], 50, display_name = "alpha")
 curve(dlnorm(x, log(90), 0.5),
       add = TRUE,
       col = "blue",
       lwd = 2)
+abline(v = 50, col = "red", lwd = 2)
 
 util$plot_expectand_pushforward(samples[['alpha_site[1]']], 50, display_name = "alpha_site[1]")
 curve(dnorm(x, 0, 10),
       add = TRUE,
       col = "blue",
       lwd = 2)
+abline(v = 0, col = "red", lwd = 2)
 
 util$plot_expectand_pushforward(samples[['beta_dbh']], 50, display_name = "beta_dbh")
 curve(dnorm(x, 0, 1),
@@ -103,6 +106,7 @@ curve(dnorm(x, 0, 1),
       col = "blue",
       lwd = 2,
       lty = 2)
+abline(v = 0.5, col = "red", lwd = 2)
 
 util$plot_expectand_pushforward(samples[['beta_GST']], 50, display_name = "beta_GST")
 curve(dnorm(x, 0, 5),
@@ -110,30 +114,35 @@ curve(dnorm(x, 0, 5),
       col = "blue",
       lwd = 2,
       lty = 2)
+abline(v = 0.5, col = "red", lwd = 2)
 
 util$plot_expectand_pushforward(samples[['beta_growth1']], 50, display_name = "beta_growth1")
 curve(dlnorm(x, 1, 0.5),
       add = TRUE,
       col = "blue",
       lwd = 2)
+abline(v = 0.04, col = "red", lwd = 2)
 
-util$plot_expectand_pushforward(samples[['beta_growth2']], 50, display_name = "beta_growth2")
+util$plot_expectand_pushforward(samples[['beta_growth2']], 50, display_name = "beta_growth2", flim=c(1.5,3.5))
 curve(dlnorm(x, 1, 0.5),
       add=TRUE, col="blue", lwd=2, lty=2)
+abline(v = 3.2, col = "red", lwd = 2)
 
-util$plot_expectand_pushforward(samples[['gamma']], 50, display_name = "gamma")
+util$plot_expectand_pushforward(samples[['theta']], 50, display_name = "theta")
+curve(dnorm(x, 0, 1),
+      add = TRUE,
+      col = "blue",
+      lwd = 2,
+      lty = 2)
+abline(v = 0.01, col = "red", lwd = 2)
+
+util$plot_expectand_pushforward(samples[['phi_sc']], 50, display_name = "phi")
 curve(dnorm(x, 0, 5),
       add = TRUE,
       col = "blue",
       lwd = 2,
       lty = 2)
-
-util$plot_expectand_pushforward(samples[['phi']], 50, display_name = "phi")
-curve(dnorm(x, 0, 5),
-      add = TRUE,
-      col = "blue",
-      lwd = 2,
-      lty = 2)
+abline(v = 2, col = "red", lwd = 2)
 
 util$plot_expectand_pushforward(samples[['sigma']], 50, display_name = "sigma")
 curve(dnorm(x, 0, 5),
@@ -141,61 +150,48 @@ curve(dnorm(x, 0, 5),
       col = "blue",
       lwd = 2,
       lty = 2)
+abline(v = 0.2, col = "red", lwd = 2)
 dev.off()
 
+rm(list = ls())
 ###for generative model###
-N <- 20
+N <- 10
+NSite <- 3
+
 DBH <- runif(N, 20, 40)
 GST <- runif(N, 15, 25)
+site <- sample(1:NSite, N, replace = TRUE)
 
-C <- numeric(N)
-gamma <- runif(N, 0.1, 0.5)
-
-G <- numeric(N)
-G[1] <- 48
-R <- numeric(N)
-R[1] <- 32
-
-rw <- numeric(N)
-rw[1] <- 0.2
-sc <- numeric(N)
-sc[1] <- 1000
-
-# true parameters
 alpha <- 50
+alpha_site <- c(0, 20, -10)
 beta_dbh <- 0.5
 beta_GST <- 0.5
-sigma_C <- 5
+sigma_c <- 0.1
+#beta_GSP <- 0.3
+beta_growth1 <- 0.04
+beta_growth2 <- 3.2
+gamma <- 0.5
+sigma_rw <- 0.5
+phi_sc <- 2
+theta <- 0.01
 
-theta <- 0.005
-sigma_rw <- 0.1
 
-eta <- 50
-
-
-for(n in 1:N){
-
-  muC <- alpha + beta_dbh*DBH[n] + beta_GST*GST[n]
-  C[n] <- rnorm(1, muC, sigma_C)
-  
+  Carb <- rlnorm(N, meanlog = log(alpha + alpha_site[site] + beta_dbh * DBH + beta_GST * GST), sdlog = sigma_c)
   
   # carbon allocation
-  G[n] <- (1-gamma[n]) * C[n]
-  R[n] <- gamma[n] * C[n]
+  G_n <- (1-gamma) * Carb
+  R_n <- gamma * Carb
   
   # ring width
-  rw[n] <- rnorm(1, theta*G[n], sigma_rw)
+  rw <- rlnorm(N, meanlog = log(((G_n / beta_growth1 + DBH^beta_growth2)^(1 / beta_growth2) - DBH) / 2), sdlog = sigma_rw)
   
   # seed counts
-  sc[n] <- rpois(1, eta*R[n])
-  
-  # update DBH
-  DBH[n] <- DBH[n-1] + rw[n]
-  
-}
+  sc <- rnegbin(N, mu = R_n/theta, theta = phi_sc)
 
 stanData <- list(
   N = N,
+  NSite = NSite,
+  site = site,
   sc = sc,
   rw = rw,
   DBH = DBH,
@@ -204,5 +200,171 @@ stanData <- list(
 
 mod <- stan_model(file='stan/tradeOffGenerative.stan')
 
-
 fit <- stan(file='stan/tradeOffGenerative.stan', data=stanData, seed=112234, control=list(adapt_delta=0.99))
+diagnostics <- util$extract_hmc_diagnostics(fit)
+
+print(util$check_all_hmc_diagnostics(diagnostics))
+
+
+samples <- util$extract_expectand_vals(fit)
+names <- c(grep('alpha', names(samples), value = TRUE),
+           grep('alpha_site', names(samples), value = TRUE),
+           grep('beta_dbh', names(samples), value = TRUE),
+           grep('beta_GST', names(samples), value = TRUE),
+           grep('beta_growth1', names(samples), value = TRUE),
+           grep('beta_growth2', names(samples), value = TRUE),
+           grep('gamma', names(samples), value = TRUE),
+           grep('phi', names(samples), value = TRUE),
+           grep('sigma', names(samples), value = TRUE))
+
+base_samples <- util$filter_expectands(samples,names)
+print(util$check_all_expectand_diagnostics(base_samples))
+pdf("pairsPlotGene.pdf", height = 9, width = 9)
+par(mfrow = c(3,3))
+util$plot_div_pairs(names, names, samples, diagnostics)
+dev.off()
+
+pdf("figures/priorPosteriorPlot.pdf", height = 9, width = 9)
+par(mfrow = c(3,3))
+util$plot_expectand_pushforward(samples[['alpha']], 50, display_name = "alpha")
+curve(dlnorm(x, log(90), 0.5),
+      add = TRUE,
+      col = "blue",
+      lwd = 2)
+abline(v = 50, col = "red", lwd = 2)
+
+util$plot_expectand_pushforward(samples[['alpha_site[1]']], 50, display_name = "alpha_site[1]")
+curve(dnorm(x, 0, 10),
+      add = TRUE,
+      col = "blue",
+      lwd = 2)
+abline(v = 0, col = "red", lwd = 2)
+
+util$plot_expectand_pushforward(samples[['beta_dbh']], 50, display_name = "beta_dbh")
+curve(dnorm(x, 0, 1),
+      add = TRUE,
+      col = "blue",
+      lwd = 2,
+      lty = 2)
+abline(v = 0.5, col = "red", lwd = 2)
+
+util$plot_expectand_pushforward(samples[['beta_GST']], 50, display_name = "beta_GST")
+curve(dnorm(x, 0, 5),
+      add = TRUE,
+      col = "blue",
+      lwd = 2,
+      lty = 2)
+abline(v = 0.5, col = "red", lwd = 2)
+
+util$plot_expectand_pushforward(samples[['beta_growth1']], 50, display_name = "beta_growth1")
+curve(dlnorm(x, 1, 0.5),
+      add = TRUE,
+      col = "blue",
+      lwd = 2)
+abline(v = 0.04, col = "red", lwd = 2)
+
+util$plot_expectand_pushforward(samples[['beta_growth2']], 50, display_name = "beta_growth2", flim=c(1.5,3.5))
+curve(dlnorm(x, 1, 0.5),
+      add=TRUE, col="blue", lwd=2, lty=2)
+abline(v = 3.2, col = "red", lwd = 2)
+
+util$plot_expectand_pushforward(samples[['phi_sc']], 50, display_name = "phi_sc")
+curve(dgamma(x, 2, 0.1),
+      add = TRUE,
+      col = "blue",
+      lwd = 2,
+      lty = 2)
+abline(v = 2, col = "red", lwd = 2)
+
+util$plot_expectand_pushforward(samples[['sigma_c']], 50, display_name = "sigma_c")
+curve(dnorm(x, 0, 1),
+      add = TRUE,
+      col = "blue",
+      lwd = 2,
+      lty = 2)
+abline(v = 0.1, col = "red", lwd = 2)
+
+util$plot_expectand_pushforward(samples[['sigma_rw']], 50, display_name = "sigma_rw")
+curve(dnorm(x, 0, 1),
+      add = TRUE,
+      col = "blue",
+      lwd = 2,
+      lty = 2)
+abline(v = 0.5, col = "red", lwd = 2)
+
+util$plot_expectand_pushforward(samples[['gamma[1]']], 50, display_name = "gamma[1]")
+curve(dnorm(x, 0, 1),
+      add = TRUE,
+      col = "blue",
+      lwd = 2,
+      lty = 2)
+abline(v = 0.5, col = "red", lwd = 2)
+
+util$plot_expectand_pushforward(samples[['gamma[2]']], 50, display_name = "gamma[2]")
+curve(dnorm(x, 0, 1),
+      add = TRUE,
+      col = "blue",
+      lwd = 2,
+      lty = 2)
+abline(v = 0.5, col = "red", lwd = 2)
+
+util$plot_expectand_pushforward(samples[['gamma[3]']], 50, display_name = "gamma[3]")
+curve(dnorm(x, 0, 1),
+      add = TRUE,
+      col = "blue",
+      lwd = 2,
+      lty = 2)
+abline(v = 0.5, col = "red", lwd = 2)
+
+util$plot_expectand_pushforward(samples[['gamma[4]']], 50, display_name = "gamma[4]")
+curve(dnorm(x, 0, 1),
+      add = TRUE,
+      col = "blue",
+      lwd = 2,
+      lty = 2)
+abline(v = 0.5, col = "red", lwd = 2)
+
+util$plot_expectand_pushforward(samples[['gamma[5]']], 50, display_name = "gamma[5]")
+curve(dnorm(x, 0, 1),
+      add = TRUE,
+      col = "blue",
+      lwd = 2,
+      lty = 2)
+abline(v = 0.5, col = "red", lwd = 2)
+
+util$plot_expectand_pushforward(samples[['gamma[6]']], 50, display_name = "gamma[6]")
+curve(dnorm(x, 0, 1),
+      add = TRUE,
+      col = "blue",
+      lwd = 2,
+      lty = 2)
+abline(v = 0.5, col = "red", lwd = 2)
+
+util$plot_expectand_pushforward(samples[['gamma[7]']], 50, display_name = "gamma[7]")
+curve(dnorm(x, 0, 1),
+      add = TRUE,
+      col = "blue",
+      lwd = 2,
+      lty = 2)
+abline(v = 0.5, col = "red", lwd = 2)
+
+util$plot_expectand_pushforward(samples[['gamma[8]']], 50, display_name = "gamma[8]")
+curve(dnorm(x, 0, 1),
+      add = TRUE,
+      col = "blue",
+      lwd = 2,
+      lty = 2)
+abline(v = 0.5, col = "red", lwd = 2)
+
+util$plot_expectand_pushforward(samples[['gamma[9]']], 50, display_name = "gamma[9]")
+curve(dnorm(x, 0, 1),
+      add = TRUE,
+      col = "blue",
+      lwd = 2,
+      lty = 2)
+abline(v = 0.5, col = "red", lwd = 2)
+
+
+
+dev.off()
+
