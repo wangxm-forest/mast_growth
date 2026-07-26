bc899e0bfeeae9aa7073fbaccb82212b40f28314
wangxm-forest <wangxiaomao0209@gmail.com>
2026-07-25 21:56:56 -0700

    explore the tree ring data I have for one stand one sp

---
+++ analyses/dataPrep.R
@@ -0,0 +1,8 @@
+###################Started by Mao####################
+###################July 23,2026######################
+
+library("dplR")
+
+setwd("C:/PhD/scanning/MORA_2024/AB08Measured")
+
+AB08THSE <- read.rwl("AB08_TSHE_dated.rwl", format = "auto")
