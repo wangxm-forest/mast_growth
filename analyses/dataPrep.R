cb1eaad60b3ef88556214cc4a88441933ab58024
wangxm-forest <wangxiaomao0209@gmail.com>
2026-07-26 22:42:52 -0700

    read and clean DBH data

--- analyses/dataPrep.R
+++ analyses/dataPrep.R
@@ -2,7 +2,19 @@
 ###################July 23,2026######################
 
 library("dplR")
+library("dplyr")
+setwd("C:/PhD/Project/PhD_thesis/mast_growth")
 
-setwd("C:/PhD/scanning/MORA_2024/AB08Measured")
+###Read in the ring width data
+AB08THSE <- read.rwl("data/measurement/crossDated/AB08_TSHE_dated.rwl", format = "auto")
 
-AB08THSE <- read.rwl("AB08_TSHE_dated.rwl", format = "auto")
+###Read in the DBH data
+dbh <- read.csv("data/dbhMORA.csv",header = TRUE)
+
+mora_stand <- c("AB08", "AV06", "TO04", "TA01", "AO03", "AG05", "AE10", "AM16")
+dbh <- dbh[dbh$STANDID %in% mora_stand, ]
+
+latest_dbh <- do.call(rbind, lapply(split(dbh, list(dbh$STANDID, dbh$TAG)), function(x) {
+  x[which.max(x$YEAR), ]
+}))
+
