3a1627915cacee7321efc8c10fed69dde3ced9a8
wangxm-forest <wangxiaomao0209@gmail.com>
2026-08-01 22:30:46 -0700

    fixed code for data preparation

--- analyses/dataPrep.R
+++ analyses/dataPrep.R
@@ -8,6 +8,20 @@ setwd("C:/PhD/Project/PhD_thesis/mast_growth")
 ###Read in the ring width data
 AB08THSE <- read.rwl("data/measurement/crossDated/AB08_TSHE_dated.rwl", format = "auto")
 
+treeID <- sub("\\(.*\\)$", "", colnames(AB08THSE))
+
+# Average across cores from the same tree
+ringWidth <- sapply(split(seq_along(treeID), treeID), function(i) {
+  if (length(i) == 1) {
+    AB08THSE[, i]
+  } else {
+    rowMeans(AB08THSE[, i, drop = FALSE], na.rm = TRUE)
+  }
+})
+
+# Keep row names (years)
+rownames(ringWidth) <- rownames(AB08THSE)
+
 ###Read in the DBH data
 dbh <- read.csv("data/dbhMORA.csv",header = TRUE)
 
